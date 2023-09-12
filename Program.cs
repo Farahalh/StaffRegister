38335230449359cac94e45f85cd31d3ca465cfea
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-12 16:34:38 +0200

    Ending assignment with an output that is not the user input, for output that reflects user input go back to previous commit.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             //Assignment 1: What classes needs to be included in the program?
+            //Adding 3 classes for each inout needed. Getting the wront output "System.Action", and not user input.
 
             //Assignment 2: Which attributes and methods should be included in these classes?
 
@@ -19,19 +20,19 @@
             Console.WriteLine("Hello " + displayFirstName + " " + displayLastName + " " + "your salary is " + displaySalary + ".");
         }
 
-        private static void displayFirstName() 
+        static void displayFirstName() 
         {
             Console.WriteLine("Insert your first name: ");
             string firstName = Console.ReadLine();
         }
 
-        private static void displayLastName()
+        static void displayLastName()
         {
             Console.WriteLine("Insert your last name: ");
             string lastName = Console.ReadLine();
         }
 
-        private static void displaySalary()
+        static void displaySalary()
         {
             Console.WriteLine("Insert your salary: ");
             string salary = Console.ReadLine();
