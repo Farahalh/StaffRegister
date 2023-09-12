395952d59617d83c0c2d0c956ccfd74f5b477b68
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-12 15:29:48 +0200

    1. Writing CW with input info for user. 2. Declaring string value to be user input in CR. 3. Converting string to integer after user input in CR. 4. Displaying user input with CW from user input in CR.
    CW = Console.WriteLine
    CR = Console.ReadLine

--- Program.cs
+++ Program.cs
@@ -4,11 +4,27 @@
     {
         static void Main(string[] args)
         {
-            // Assignment 1: What classes needs to be included in the programe?
+            //Assignment 1: What classes needs to be included in the programe?
 
             //Assignment 2: Which attributes and methods should be included in these classes?
 
             //Assignment 3: Write the program
+
+            Console.WriteLine("Welcome to the Staff Register");
+            Console.WriteLine("Insert youre first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Insert youre last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Insert your salary: ");
+            string salary = Console.ReadLine();
+
+            int res;
+            res = Convert.ToInt32(salary);
+
+            Console.WriteLine("Hello " + firstName + " " + lastName + " " + "youre salary is " + salary + ".");
+            Console.ReadLine(); 
         }
     }
 }
