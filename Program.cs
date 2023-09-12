6eeafa47c767798cec5eb1c525cd1d478fcf1e76
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-12 15:10:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StaffRegister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assignment 1: What classes needs to be included in the programe?
+
+            //Assignment 2: Which attributes and methods should be included in these classes?
+
+            //Assignment 3: Write the program
+        }
+    }
+}
