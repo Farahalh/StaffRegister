92e47b7e6196e2837da79dd60b68f880ec337a8b
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-12 15:34:28 +0200

    Ending assignment, the application works. Very simple structure as I am new to C#.

--- Program.cs
+++ Program.cs
@@ -4,17 +4,17 @@
     {
         static void Main(string[] args)
         {
-            //Assignment 1: What classes needs to be included in the programe?
+            //Assignment 1: What classes needs to be included in the program?
 
             //Assignment 2: Which attributes and methods should be included in these classes?
 
             //Assignment 3: Write the program
 
             Console.WriteLine("Welcome to the Staff Register");
-            Console.WriteLine("Insert youre first name: ");
+            Console.WriteLine("Insert your first name: ");
             string firstName = Console.ReadLine();
 
-            Console.WriteLine("Insert youre last name: ");
+            Console.WriteLine("Insert your last name: ");
             string lastName = Console.ReadLine();
 
             Console.WriteLine("Insert your salary: ");
@@ -23,7 +23,7 @@
             int res;
             res = Convert.ToInt32(salary);
 
-            Console.WriteLine("Hello " + firstName + " " + lastName + " " + "youre salary is " + salary + ".");
+            Console.WriteLine("Hello " + firstName + " " + lastName + " " + "your salary is " + salary + ".");
             Console.ReadLine(); 
         }
     }
